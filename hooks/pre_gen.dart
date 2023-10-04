025f757d518d763be8171a917a6d866bda182ee6
siddhant <sidhant@edupluscampus.com>
2023-10-04 12:17:29 +0530

    pre_gen changes

--- hooks/pre_gen.dart
+++ hooks/pre_gen.dart
@@ -1,3 +1,25 @@
 import 'package:mason/mason.dart';
 
-void run(HookContext context) {}
+enum CRUD_OPERATION {
+  Create,
+  Read,
+  Update,
+  Delete,
+}
+
+void run(HookContext context) {
+  bool isCrudOperation = context.logger.confirm(
+    "does this feature consist CRUD opwrations?",
+    defaultValue: false,
+  );
+
+  if (!isCrudOperation) return;
+
+  final operations = context.logger.chooseAny<CRUD_OPERATION>(
+    "Select operations",
+    choices: CRUD_OPERATION.values,
+    defaultValues: CRUD_OPERATION.values,
+  );
+
+  context.logger.write(operations.map((e) => e.name).toList().toString());
+}
